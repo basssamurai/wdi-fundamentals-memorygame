fb2868073cee9c1b60a3455c46e79efa743ac82a
Christian Lanoue <XIAN@Christians-MBP.fios-router.home>
2017-07-25 17:08:50 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 var cards = ["queen", "queen", "king", "king"];
 
-var cardsInPlay = [];
+var cardsInPlay = []
 
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log('User flipped queen');
-
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log('User flipped king');
-
-if (cardsInPlay.length === 2); 
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
-	}
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+}
 
-	else if (cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again.")
-	}
+var flipCard = function(cardId) {
+
+console.log("User flipped" + cards[0] || + "User flipped" + cards[1] || "User flipped" + cards[2] || "User flipped" + cards[3]);
+
+cardsInPlay.push(cards(0) || or cards(1) || cards(2) || cards(3) );
+
+cards(0);
+cards(2);
+
+checkForMatch();
+
+
+};
 
 
 
